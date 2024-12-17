a3d1bb1a01a48f0be35afa6365dbfead74e6aa11
navithraj-d-k <navithraj58@gmail.com>
2024-12-17 11:57:57 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,39 @@
+import React from 'react';
+import PingPongGame from './components/PingPongGame';
+import { Web3Provider } from './context/Web3Context';
 
+function App() {
+  return (
+    <Web3Provider>
+      <div className="App">
+        <header className="bg-blue-600 text-white p-4">
+          <h1 className="text-2xl font-bold">NFT Ping Pong Game</h1>
+        </header>
+        <main className="container mx-auto p-4">
+          <PingPongGame />
+        </main>
+      </div>
+    </Web3Provider>
+  );
+}
+
+export default App;import React from 'react';
+import PingPongGame from './components/PingPongGame';
+import { Web3Provider } from './context/Web3Context';
+
+function App() {
+  return (
+    <Web3Provider>
+      <div className="App">
+        <header className="bg-blue-600 text-white p-4">
+          <h1 className="text-2xl font-bold">NFT Ping Pong Game</h1>
+        </header>
+        <main className="container mx-auto p-4">
+          <PingPongGame />
+        </main>
+      </div>
+    </Web3Provider>
+  );
+}
+
+export default App;
